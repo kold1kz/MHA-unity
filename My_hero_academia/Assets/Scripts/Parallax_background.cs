5c70efbce20259b6368ea8dc2681f13e96a21735
kold1kz <kold1kz@mail.ru>
2023-05-22 01:43:06 +0300

    start lvl design

---
+++ My_hero_academia/Assets/Scripts/Parallax_background.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Parallax_background : MonoBehaviour
+{ 	 
+    public GameObject cam;
+    
+    float startPosX;
+    float startPosY;
+    
+    public float parallax;
+
+    
+    void Start()
+    {
+    	startPosX = transform.position.x;    
+    	startPosY = transform.position.y;  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distX = (cam.transform.position.x * (1 - parallax));
+        float distY = (cam.transform.position.y * (1 - parallax));
+		transform.position = new Vector3(startPosX + distX, startPosY + distY, transform.position.z); 
+	}
+}
